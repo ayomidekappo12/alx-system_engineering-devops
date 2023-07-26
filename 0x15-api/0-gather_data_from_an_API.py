ecaf8cbb1d1ede07865f420fe6268434b856bf17
ayomidekappo12 <ayomidekappo12@gmail.com>
2023-07-26 16:13:34 -0700

    added 0x15 file

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """Returns to-do list information for a given employee ID."""
-import sys
 import requests
+import sys
+
 
 def get_employee_todo_list(employee_id):
     url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
@@ -13,18 +14,21 @@ def get_employee_todo_list(employee_id):
         print(f"Error: Employee with ID {employee_id} not found.")
         return
 
-    todo_url = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
+    todo_url = f'https://jsonplaceholder.typicode.com/
+    todos?userId={employee_id}'
     response = requests.get(todo_url)
     if response.status_code == 200:
         todos_data = response.json()
     else:
-        print(f"Error: Unable to fetch TODO list for Employee ID {employee_id}.")
+        print(f"Error: Unable to fetch TODO
+                list for Employee ID {employee_id}.")
         return
 
     total_tasks = len(todos_data)
     done_tasks = sum(1 for todo in todos_data if todo['completed'])
 
-    print(f"Employee {name} is done with tasks({done_tasks}/{total_tasks}):")
+    print(f"Employee {name} is done with tasks
+            ({done_tasks}/{total_tasks}):")
     for todo in todos_data:
         if todo['completed']:
             print("\t", todo['title'])
