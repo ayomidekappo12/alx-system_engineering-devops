4afdd8f6ba6553764b485bab3d7f78e3d93fee23
ayomidekappo12 <ayomidekappo12@gmail.com>
2023-07-27 03:44:17 -0700

    added 0x15-api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """Returns to-do list information for a given employee ID."""
+
 import requests
 import sys
 
 
-def get_employee_todo_list(employee_id):
-    url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
-    response = request.get(url)
+def get_employee_todo_progress(employee_id):
+    base_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
+    response = requests.get(base_url)
     if response.status_code == 200:
         employee_data = response.json()
         name = employee_data['name']
@@ -20,26 +21,25 @@ def get_employee_todo_list(employee_id):
     if response.status_code == 200:
         todos_data = response.json()
     else:
-        print(f"Error: Unable to fetch TODO
-                list for Employee ID {employee_id}.")
+        print(f"Error: Unable to fetch TODO list for Employee
+                ID {employee_id}.")
         return
 
     total_tasks = len(todos_data)
     done_tasks = sum(1 for todo in todos_data if todo['completed'])
 
-    print(f"Employee {name} is done with tasks
-            ({done_tasks}/{total_tasks}):")
+    print(f"Employee {name} is done with tasks({done_tasks}/
+            {total_tasks}):")
     for todo in todos_data:
         if todo['completed']:
             print("\t", todo['title'])
-
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python3 script_name.py employee_id")
     else:
         try:
-             employee_id = int(sys.argv[1])
-             get_employee_todo_progress(employee_id)
+            employee_id = int(sys.argv[1])
+            get_employee_todo_progress(employee_id)
         except ValueError:
             print("Error: Invalid employee ID. Please provide an integer.")
