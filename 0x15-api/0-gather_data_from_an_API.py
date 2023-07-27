8bec7677999bf641a6653e730f5210328a820a3a
ayomidekappo12 <ayomidekappo12@gmail.com>
2023-07-27 03:56:27 -0700

    added 0x15-api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,45 +1,14 @@
 #!/usr/bin/python3
 """Returns to-do list information for a given employee ID."""
-
 import requests
 import sys
 
-
-def get_employee_todo_progress(employee_id):
-    base_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
-    response = requests.get(base_url)
-    if response.status_code == 200:
-        employee_data = response.json()
-        name = employee_data['name']
-    else:
-        print(f"Error: Employee with ID {employee_id} not found.")
-        return
-
-    todo_url = f'https://jsonplaceholder.typicode.com/
-    todos?userId={employee_id}'
-    response = requests.get(todo_url)
-    if response.status_code == 200:
-        todos_data = response.json()
-    else:
-        print(f"Error: Unable to fetch TODO list for Employee
-                ID {employee_id}.")
-        return
-
-    total_tasks = len(todos_data)
-    done_tasks = sum(1 for todo in todos_data if todo['completed'])
-
-    print(f"Employee {name} is done with tasks({done_tasks}/
-            {total_tasks}):")
-    for todo in todos_data:
-        if todo['completed']:
-            print("\t", todo['title'])
-
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 script_name.py employee_id")
-    else:
-        try:
-            employee_id = int(sys.argv[1])
-            get_employee_todo_progress(employee_id)
-        except ValueError:
-            print("Error: Invalid employee ID. Please provide an integer.")
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
